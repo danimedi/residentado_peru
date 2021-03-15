edf58b1b364a15aa355881ce4d98f0b3d65401e4
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-15 18:26:03 -0500

    create function to unify data sets

---
+++ code/unify_data_sets.R
@@ -0,0 +1,22 @@
+#' Combine data sets from different years into one big data set with all the information
+#'
+#' @param files vector of strings with the paths of the files to the CSV files
+#' @param years vector with the years of the files, they have to be the same length as 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' files <- list.files("data/clean/identified", full.names = TRUE, pattern = "postulantes")
+#' years <- 2013:2020
+#' unify_data_sets(files, years)
+#' 
+unify_data_sets <- function(files, years) {
+  # read the files in a list as characters
+  dat <- lapply(seq_along(files), function(i) {
+    dplyr::mutate(readr::read_csv(files[[i]], readr::col_types = readr::cols(.default = col_character())), 
+           year = years[[i]])
+  })
+  # bind the data sets by rows
+  dplyr::bind_rows(dat)
+}
